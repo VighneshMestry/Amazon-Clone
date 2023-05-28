cb747f6b844dd13d6f12d5dc0f1c4df63bbf88a1
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2023-05-28 18:36:37 +0530

    add to cart done

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/feautres/account/screen/account_screen.dart';
+import 'package:amazon_clone/feautres/cart/screens/cart_screen.dart';
 import 'package:amazon_clone/feautres/home/screens/home_screen.dart';
 import 'package:amazon_clone/provider/user_provider.dart';
 import 'package:badges/badges.dart' as Badges;
@@ -22,7 +23,7 @@ class _BottomBarState extends State<BottomBar> {
   List<Widget> pages = [
     const HomeScreen(),
     const AccountScreen(),
-    const Center(child: Text('Cart Page')),
+    const CartScreen(),
   ];
 
   void updatePage(int page) {
---
+++ lib/feautres/cart/widgets/quantity_inc_dec.dart
@@ -0,0 +1,75 @@
+import 'package:amazon_clone/provider/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class QuantityIncDec extends StatelessWidget {
+  final int index;
+  const QuantityIncDec({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final productCart = context.watch<UserProvider>().user.cart[index];
+    final quantity = productCart['quantity'];
+    return Container(
+          margin: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.black12,
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.remove,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black12, width: 1.5),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: Text(
+                          quantity.toString(),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.add,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
